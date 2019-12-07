b403064a253603def0691540a7b97ad551a26d4b
christinawanggg <christinawang2000@gmail.com>
2019-12-07 18:13:18 -0500

    bootstrap

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from .models import Home
+
 # Create your views here.
+def home(request):
+    jobs = Home.objects
+    return render(request, 'symptoms/home.html', {'symptoms':home})
